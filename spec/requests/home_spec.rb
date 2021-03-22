2b75d8cc536024ab2fd8ee4052dfac28e064f520
David Ramirez <david@davidrv.com>
2021-03-22 09:46:30 +0100

    fix the home specs

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    it "returns http redirect without logging in" do
       get "/home/index"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
